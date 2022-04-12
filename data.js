1d22e8e832b3e89767b93c939acb6713106f4e0b
Cbababa <100921907+Cbababa@users.noreply.github.com>
2022-04-12 17:18:03 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-vr-r7-master-recovered-recovered-2e-chi",
+      "name": "360-VR R7 (Master)-Recovered-Recovered 2e (Chi)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5788454512386885,
+        "pitch": 0.20804652207374374,
+        "fov": 0.9789053594153913
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
